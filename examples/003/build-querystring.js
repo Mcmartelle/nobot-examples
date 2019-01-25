2a6dc4fce4281be35ee401219f55e49397edf3e2
Matthew Martelle <matthew.martelle@aofl.com>
2019-01-24 21:48:47 -0800

    commiting progress

--- examples/003/build-querystring.js
+++ examples/003/build-querystring.js
@@ -1,1 +1,13 @@
 const querystring = require('querystring');
+
+const apiHost = 'http://jira.my-company.com/rest/api/latest/search?jql=';
+
+const jqlParams = {
+  assignee: 'first.last',
+  startAt: 2,
+  maxResults: 2
+};
+
+const apiUrl = `${apiHost}"${querystring.stringify(jqlParams)}"`;
+
+console.log(`My JQL api call is: ${apiUrl}`);
