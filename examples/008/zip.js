7b5fc691f2637a5a6ae13e9b9393d8052c24361a
Matthew Martelle <matthew.martelle@aofl.com>
2019-01-25 19:37:58 -0800

    commiting progress

--- examples/008/zip.js
+++ examples/008/zip.js
@@ -1,3 +1,16 @@
 const archiver = require('archiver');
 const fs = require('fs');
 const path = require('path');
+
+const ZLIB_BEST_COMPRESSION = 9;
+// create a file to stream archive data to.
+const zipPath = path.join(__dirname, 'files.zip');
+const output = fs.createWriteStream(zipPath);
+const archive = archiver('zip', {
+  zlib: { level: ZLIB_BEST_COMPRESSION }
+});
+
+output.on('close', () => {
+  console.log(`Total bytes: ${archive.pointer()}`);
+  console.log('archiving has now finished');
+});
